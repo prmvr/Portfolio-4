7788cd8498b6b608e36ccd2c55e9cea8a4a20ba0
Pkaile <69093808+Pkaile@users.noreply.github.com>
2021-03-13 19:04:40 -0800

    added slideshow

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const slides = document.getElementsByClassName('project');
+const slides = document.querySelectorAll('.project');
+const slideDisplay = document.querySelector('#amount');
 let curSlide = 0;
 const totalSlides = slides.length;
-console.log(totalSlides );
 document.getElementById('left-slider-btn').addEventListener('click', function (e){
-moveSlidesBack();
+moveSlideBack();
 });
 document.getElementById('right-slider-btn').addEventListener('click', function(e) {
-moveSlidesForward();
+moveSlideForward();
 });
+updateSlides();
+function moveSlideBack(){
+    if(curSlide === 0){
+    curSlide = 0;
+    } else curSlide--;
+    return updateSlides();
+}
+function moveSlideForward(){
+    if(curSlide === totalSlides - 1) {
+        curSlide = 0;
+    }
+    else curSlide++;
+    return updateSlides();
+}
+function updateSlides(){
+    for(let slide of slides){
+        slide.classList.remove('is-active');
+    }
+    slides[curSlide].classList.add('is-active');
+    slideDisplay.innerHTML = `( ${curSlide + 1} / ${totalSlides} )`;
+}
